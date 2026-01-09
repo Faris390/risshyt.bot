e20691392ce668967d13f0cc4b56b818e564a10b
Faris390 <73607372+Faris390@users.noreply.github.com>
2026-01-09 10:16:05 +0700

    Initialize global settings for Risshyt Bot
    Added global configuration settings for the bot.

--- settings.js
+++ settings.js
@@ -1,1 +1,24 @@
+global.author = 'Risshyt';
+global.packname = 'Risshyt.Bot';
 
+global.owner = [
+    ['6285815953668', 'Risshyt Owner', true],
+    ['6287884358475', 'Risshyt Backup', false] 
+];
+
+// Nomor Bot untuk Login Pairing Code (PENTING: Ganti dengan nomor bot)
+global.number_bot = '62878xxxxxxxx'; 
+global.pairing_code = true; 
+
+// --- Path File & Database ---
+// Folder tempat sesi Baileys akan disimpan
+global.session_folder = 'risshyt-session'; 
+
+// Database Path
+global.tempatStore = './data/store.json'; 
+global.tempatDB = './data/db.json';       
+
+global.my = {
+    gh: 'https://github.com/Faris390', 
+    ch: '6285815953668@newsletter'     
+}
